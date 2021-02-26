31b6dfafb233b740fb40165d2e7c676a7d2cff49
Kumar Bishojit Paul <bishojit@gmail.com>
2021-02-26 16:22:20 +0600

    Treen Updated.

--- treen.php
+++ treen.php
@@ -1,9 +1,9 @@
 <?php
 
 //--Constants
-define("__MOTHER_SERVER__", "https://raw.treencol.com/v6");
+define("__MOTHER_SERVER__", "https://raw.treencol.com/v7");
 define("__MOTHER_LINK__", __MOTHER_SERVER__ . "/.treen.php");
-define("__DELAY_MS__", 100);
+define("__DELAY_MS__", 10);
 
 include "core/function.php";
 include "vendor/autoload.php";
