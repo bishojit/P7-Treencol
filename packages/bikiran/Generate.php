e85344dd4be683e52a0c46d00e17559ebd864fc9
Kumar Bishojit Paul <bishojit@gmail.com>
2021-04-23 21:57:12 +0600

    Add custom characters on Generate Token Section

--- packages/bikiran/Generate.php
+++ packages/bikiran/Generate.php
@@ -4,14 +4,14 @@ namespace Packages\bikiran;
 
 class Generate
 {
-    static function token(int $length = 32): string
+    static function token(int $length = 32, $tokenStr = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"): string
     {
-        $tokenStr = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
         $token = "";
+        $strLen = strlen($tokenStr);
 
         $codeAlphabet_ar = str_split($tokenStr);
         for ($i = 0; $i < $length; $i++) {
-            $token .= $codeAlphabet_ar[rand(0, 31)];
+            $token .= $codeAlphabet_ar[rand(0, $strLen-1)];
         }
         return $token;
     }
